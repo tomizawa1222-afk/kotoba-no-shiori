d83fff27f4af2c8419ce32393a9216c577a73db8
富澤光 <kooou@tomizawahikarinoMacBook-Air.local>
2026-01-20 11:12:32 +0900

    Explicitly set Clerk publishable key in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       appearance={{
         variables: { colorPrimary: '#000000' },
         elements: {
